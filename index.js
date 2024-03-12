5a200fd360f21844fae724f7e10f5cbe4ca2e8ea
kibaara-wangu <kibaarawangu@gmail.com>
2024-03-12 22:12:29 +0300

    Javascript practice

---
+++ index.js
@@ -0,0 +1,41 @@
+// Concatenate the strings "Hello" and "World" in JavaScript to create the message "Hello World."
+let a = "Hello";
+let b = "World";
+let greeting = a+" "+b;
+console.log({greeting});
+
+
+// Use the + operator to combine the variables firstName and lastName into a full name.
+firstName = "Jane";
+lastName = "Kibaara";
+let names = firstName +" "+lastName;
+console.log({names});
+
+
+// Create a template literal in JavaScript to display the current year dynamically. For example, "The current year is 2024."
+let k = "Joy";
+let m = "Maish";
+let sentence = `${k} ${m},The current year is 2024`;
+console.log({sentence});
+
+// Use string interpolation to insert the value of a variable age into the string "I am years old."
+let age = 23;
+let identity = `I am ${age} years old`;
+console.log({identity}); 
+
+// Given the string "JavaScript", use indexing to retrieve and print the first and last characters.
+let f = "JavaScript";
+console.log('index', f.indexOf('J'));
+console.log('index', f.indexOf('t'));
+
+// Access the third element in an array and print its value.
+let num1 = 12;
+let num2 = 10;
+let add = num1 + num2;
+console.log({add});
+
+// Multiply the variable quantity by the variable price to calculate the total cost.
+let p = 10;
+let q = 250;
+let Multiply = p * q;
+console.log({Multiply});
